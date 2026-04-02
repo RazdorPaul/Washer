2aaac862252731b437f018b755f94f4aa7275bcc
Раздорожный Павел <kaisergars@gmail.com>
2026-04-03 00:38:37 +0900

    обавлены классы услуги и работники

---
+++ src/main/java/my/washer/core/Services.java
@@ -0,0 +1,9 @@
+package main.java.my.washer.core;
+
+import java.util.Map;
+
+public class Services {
+    private String service;
+    private int timeService;
+    private int price;
+}
---
+++ src/main/java/my/washer/core/Worker.java
@@ -0,0 +1,10 @@
+package main.java.my.washer.core;
+
+public class Worker {
+    String name;
+    String workSince;
+    Long phone;
+    Integer hoursInMonth;
+    Integer salary;
+
+}
