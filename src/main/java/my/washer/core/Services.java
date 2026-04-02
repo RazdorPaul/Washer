a05ba182caa3f264ca2ecf57236f93b863785173
Раздорожный Павел <kaisergars@gmail.com>
2026-04-03 02:50:29 +0900

    модификация интерфейса

--- src/main/java/my/washer/core/Services.java
+++ src/main/java/my/washer/core/Services.java
@@ -6,4 +6,6 @@ public class Services {
     private String service;
     private int timeService;
     private int price;
+    private String typeChemicalResource;
+    private Integer consumptionRate;
 }
